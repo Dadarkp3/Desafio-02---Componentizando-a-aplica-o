d7c55bd4fbe1b54e527b38d7f7d4474ece41a09d
Daiane Silva <adm.daianesilva@gmail.com>
2021-10-24 16:08:00 -0300

    chore: completing project

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+interface sideBarProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+const SideBar = ({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: sideBarProps) => {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default SideBar;
